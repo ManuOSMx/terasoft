a42041c24fc343c5f2108bb0f1f2350550d0ca75
Manuel Ortiz <manuel4612@hotmail.com>
2021-08-02 14:51:00 -0500

    commit Main

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-console.log("Hola a todos");
+console.log("¿Como están? Los saludo desde la rama main");
 var editar = document.getElementById("edit");
+var branchName = "Rama Main";
+editar.innerHTML = "Hola saludo desde " + branchName;
--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
-console.log("Hola a todos");
+console.log("¿Como están? Los saludo desde la rama main");
 const editar = document.getElementById("edit");
+const branchName : String = "Rama Main";
+
+editar.innerHTML = `Hola saludo desde ${branchName}`;
 
